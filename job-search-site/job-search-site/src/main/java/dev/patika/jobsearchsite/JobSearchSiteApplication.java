523137977cc13cfb6ca00092f81ec732bb1d902f
Kadir Şensoy <ksensoy@isbak.com.tr>
2022-07-15 01:00:29 +0300

    project created

---
+++ job-search-site/job-search-site/src/main/java/dev/patika/jobsearchsite/JobSearchSiteApplication.java
@@ -0,0 +1,13 @@
+package dev.patika.jobsearchsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSearchSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSearchSiteApplication.class, args);
+	}
+
+}
---
+++ job-search-site/job-search-site/src/test/java/dev/patika/jobsearchsite/JobSearchSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.jobsearchsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
