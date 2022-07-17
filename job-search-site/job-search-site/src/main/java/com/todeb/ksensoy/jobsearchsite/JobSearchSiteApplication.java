509f009f301008d31f146d56df0de1c9f381e293
Kadir Şensoy <ksensoy@isbak.com.tr>
2022-07-17 21:27:00 +0300

    project edited

--- job-search-site/job-search-site/src/main/java/com/todeb/ksensoy/jobsearchsite/JobSearchSiteApplication.java
+++ job-search-site/job-search-site/src/main/java/com/todeb/ksensoy/jobsearchsite/JobSearchSiteApplication.java
@@ -1,4 +1,4 @@
-package dev.patika.jobsearchsite;
+package com.todeb.ksensoy.jobsearchsite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobSearchSiteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JobSearchSiteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JobSearchSiteApplication.class, args);
+    }
 
 }
--- job-search-site/job-search-site/src/test/java/com/todeb/ksensoy/jobsearchsite/JobSearchSiteApplicationTests.java
+++ job-search-site/job-search-site/src/test/java/com/todeb/ksensoy/jobsearchsite/JobSearchSiteApplicationTests.java
@@ -1,4 +1,4 @@
-package dev.patika.jobsearchsite;
+package com.todeb.ksensoy.jobsearchsite;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JobSearchSiteApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
